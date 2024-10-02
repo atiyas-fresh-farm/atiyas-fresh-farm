fd034a7ecafc95398605a8a72be8e8960e74f055
Rayyan Ahmed <5747484+rayyanQ@users.noreply.github.com>
2024-10-02 19:48:24 -0400

    setup website layout and min height

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -39,8 +39,10 @@ export default function RootLayout({
         >
           <div className="flex flex-col justify-start items-start">
             <Header />
-            {children}
-            <Footer />
+            <main className="w-full min-h-screen pt-16 flex flex-col justify-between">
+              {children}
+              <Footer />
+            </main>
           </div>
         </ThemeProvider>
       </body>
--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer>
+    <footer className="w-full h-16 px-4 flex flex-row justify-between items-center border-t">
       <p>© 2021 All rights reserved.</p>
     </footer>
   );

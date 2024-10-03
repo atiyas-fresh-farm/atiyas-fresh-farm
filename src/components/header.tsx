fd0ee1a52356d016df517d66afdab21f572d93b5
Rayyan Ahmed <5747484+rayyanQ@users.noreply.github.com>
2024-10-03 10:21:03 -0400

    add search bar with outline

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,10 +1,18 @@
 import { ThemeToggle } from "@/components/theme-toggle";
+import { SearchBar } from "@/components/search/search-bar";
 
 const Header = () => {
   return (
-    <header className="w-full h-16 px-4 flex flex-row justify-between items-center border-b fixed">
-      <h1>Atiyas Fresh Farm</h1>
-      <ThemeToggle />
+    <header className="w-full h-16 px-4 grid grid-cols-4 items-center border-b fixed">
+      <span className="col-span-1">
+        <h1>Atiyas Fresh Farm</h1>
+      </span>
+      <span className="col-span-2">
+        <SearchBar />
+      </span>
+      <span className="col-span-1 flex flex-col items-end">
+        <ThemeToggle />
+      </span>
     </header>
   );
 }
---
+++ src/components/search/search-bar.tsx
@@ -0,0 +1,9 @@
+const SearchBar = () => {
+  return (
+    <div className="w-full px-2 py-1 border rounded-full">
+      <input className="w-min-[30rem] w-full h-10 p-2 bg-transparent outline-none" type="text" placeholder="Search" />
+    </div>
+  )
+}
+
+export { SearchBar };

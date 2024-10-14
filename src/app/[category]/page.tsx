97cfb1cf4aca1c5985e1905e6afdd47c4e0da7a3
Rayyan Ahmed <5747484+rayyanQ@users.noreply.github.com>
2024-10-13 20:18:31 -0400

    add sidebar to categories

--- src/app/[category]/page.tsx
+++ src/app/[category]/page.tsx
@@ -1,7 +1,14 @@
 const Category = () => {
   return (
-    <div>
-      <h1>Category</h1>
+    <div className="w-full flex justify-center">
+      <main className="container flex flex-row justify-start items-start pt-10">
+        <div className="min-w-96 ">
+          <div className="w-64 h-40 bg-neutral-300 rounded-md"></div>
+          <div className="w-64 h-40 bg-neutral-300 rounded-md"></div>
+          <div className="w-64 h-40 bg-neutral-300 rounded-md"></div>
+        </div>
+        <div></div>
+      </main>
     </div>
   );
 }

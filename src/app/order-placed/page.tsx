c6e11931ab46d5fd114d6533dc471a2a093d328a
Rayyan Ahmed <5747484+rayyanQ@users.noreply.github.com>
2024-10-19 01:25:02 -0400

    setup order-placed page with placeholder content

--- src/app/order-placed/page.tsx
+++ src/app/order-placed/page.tsx
@@ -1,7 +1,24 @@
+import { H2, P } from "@/components/ui/typography";
+import { Badge } from "@/components/ui/badge";
+import Link from "next/link";
+
 const OrderPlaced = () => {
   return (
-    <div>
-      <h1>OrderPlaced</h1>
+    <div className="w-full flex justify-center">
+      <main className="container flex flex-col justify-start items-start pt-10">
+        <H2>Order Placed</H2>
+        <div className="w-full flex flex-row justify-between items-center mb-4">
+          <P>
+            Thank you for placing your order. You will receive an email with your order details. <br />
+            We will call if any of the items you ordered are out of stock.
+          </P>
+          <Badge>Confirming your order</Badge>
+        </div>
+        <div className="flex flex-row space-x-4">
+          <Link href="/" className="underline">Continue shopping</Link>
+          <Link href="/order-history" className="underline">Order History</Link>
+        </div>
+      </main>
     </div>
   );
 }

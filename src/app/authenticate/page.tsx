ab56ace8ed978c93d3fdfd8d148bb12d1ee4c886
Rayyan Ahmed <5747484+rayyanQ@users.noreply.github.com>
2024-10-20 01:11:37 -0400

    combine signup and login pages and create skeleton

---
+++ src/app/authenticate/page.tsx
@@ -0,0 +1,81 @@
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import {
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger,
+} from "@/components/ui/tabs"
+
+
+const Signup = () => {
+  return (
+    <div className="w-full flex justify-center">
+      <main className="container h-full flex flex-col justify-center items-center pt-10">
+        <Tabs defaultValue="account" className="w-[400px]">
+          <TabsList className="grid w-full grid-cols-2">
+            <TabsTrigger value="signup">Sign Up</TabsTrigger>
+            <TabsTrigger value="login">Log In</TabsTrigger>
+          </TabsList>
+          <TabsContent value="signup">
+            <Card>
+              <CardHeader>
+                <CardTitle>Sign Up</CardTitle>
+                <CardDescription>
+                  Create a new account.
+                </CardDescription>
+              </CardHeader>
+              <CardContent className="space-y-2">
+                <div className="space-y-1">
+                  <Label htmlFor="email">Email</Label>
+                  <Input id="email" type="email" placeholder="Email Address" />
+                </div>
+                <div className="space-y-1">
+                  <Label htmlFor="password">Password</Label>
+                  <Input id="password" type="password" placeholder="Password" />
+                </div>
+              </CardContent>
+              <CardFooter>
+                <Button>Create Account</Button>
+              </CardFooter>
+            </Card>
+          </TabsContent>
+          <TabsContent value="login">
+            <Card>
+              <CardHeader>
+                <CardTitle>Log In</CardTitle>
+                <CardDescription>
+                  Log into your existing account.
+                </CardDescription>
+              </CardHeader>
+              <CardContent className="space-y-2">
+                <div className="space-y-1">
+                  <Label htmlFor="Email">Email</Label>
+                  <Input id="email" type="email" placeholder="Email" />
+                </div>
+                <div className="space-y-1">
+                  <Label htmlFor="password">Password</Label>
+                  <Input id="password" type="password" placeholder="Password" />
+                </div>
+              </CardContent>
+              <CardFooter>
+                <Button>Login to your account</Button>
+              </CardFooter>
+            </Card>
+          </TabsContent>
+        </Tabs>
+      </main>
+    </div>
+  )
+}
+
+export default Signup;

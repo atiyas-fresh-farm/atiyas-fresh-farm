4f5e6f667247a70e13fc381eadc0bea5442b4b91
Rayyan Ahmed <5747484+rayyanQ@users.noreply.github.com>
2024-10-20 00:36:54 -0400

    add search results skeleton

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,7 +1,39 @@
+import { Large, Small } from '@/components/ui/typography';
+
 const Search = () => {
   return (
-    <div>
-      <h1>Search</h1>
+    <div className="w-full flex justify-center">
+      <main className="container flex flex-col justify-start items-start pt-10">
+        <Large>Search Results</Large>
+        <Small>Showing 1-20 of 100 results</Small>
+        <br />
+        <div className="w-full flex flex-row flex-wrap justify-between items-start">
+          <div className="w-48 h-72 bg-neutral-300 mx-3 mb-6 rounded-md"></div>
+          <div className="w-48 h-72 bg-neutral-300 mx-3 mb-6 rounded-md"></div>
+          <div className="w-48 h-72 bg-neutral-300 mx-3 mb-6 rounded-md"></div>
+          <div className="w-48 h-72 bg-neutral-300 mx-3 mb-6 rounded-md"></div>
+          <div className="w-48 h-72 bg-neutral-300 mx-3 mb-6 rounded-md"></div>
+          <div className="w-48 h-72 bg-neutral-300 mx-3 mb-6 rounded-md"></div>
+          <div className="w-48 h-72 bg-neutral-300 mx-3 mb-6 rounded-md"></div>
+
+          <div className="w-48 h-72 bg-neutral-300 mx-3 mb-6 rounded-md"></div>
+          <div className="w-48 h-72 bg-neutral-300 mx-3 mb-6 rounded-md"></div>
+          <div className="w-48 h-72 bg-neutral-300 mx-3 mb-6 rounded-md"></div>
+          <div className="w-48 h-72 bg-neutral-300 mx-3 mb-6 rounded-md"></div>
+          <div className="w-48 h-72 bg-neutral-300 mx-3 mb-6 rounded-md"></div>
+          <div className="w-48 h-72 bg-neutral-300 mx-3 mb-6 rounded-md"></div>
+          <div className="w-48 h-72 bg-neutral-300 mx-3 mb-6 rounded-md"></div>
+
+          <div className="w-48 h-72 bg-neutral-300 mx-3 mb-6 rounded-md"></div>
+          <div className="w-48 h-72 bg-neutral-300 mx-3 mb-6 rounded-md"></div>
+          <div className="w-48 h-72 bg-neutral-300 mx-3 mb-6 rounded-md"></div>
+          <div className="w-48 h-72 bg-neutral-300 mx-3 mb-6 rounded-md"></div>
+          <div className="w-48 h-72 bg-neutral-300 mx-3 mb-6 rounded-md"></div>
+          <div className="w-48 h-72 bg-neutral-300 mx-3 mb-6 rounded-md"></div>
+          <div className="w-48 h-72 bg-neutral-300 mx-3 mb-6 rounded-md"></div>
+
+        </div>
+      </main>
     </div>
   );
 }

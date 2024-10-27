1856d838dac94159a272d8410c59c0be5feee2a9
Rayyan Ahmed <5747484+rayyanQ@users.noreply.github.com>
2024-10-27 14:49:32 -0400

    add query params in url when user types in search bar

--- src/components/search/search-bar.tsx
+++ src/components/search/search-bar.tsx
@@ -1,10 +1,35 @@
+'use client'
+
 import { Search } from "lucide-react";
+import { useSearchParams, usePathname, useRouter } from 'next/navigation';
+//import { useDebouncedCallback } from 'use-debounce';
 
 const SearchBar = () => {
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
+
+  const handleSearch = (term: string) => {
+    const params = new URLSearchParams(searchParams);
+    if (term) {
+      params.set('query', term);
+    } else {
+      params.delete('query');
+    }
+    replace(`${pathname}?${params.toString()}`);
+  };
+
   return (
     <div className="w-full h-full flex flex-row justify-center items-center px-4 py-1 border border-neutral-300 rounded-full">
       <Search size={24} className="text-neutral-500" />
-      <input className="w-min-[30rem] w-full h-10 p-2 bg-transparent outline-none" type="text" placeholder="Search" />
+      <input
+        className="w-min-[30rem] w-full h-10 p-2 bg-transparent outline-none"
+        type="text" placeholder="Search"
+        onChange={(e) => {
+          handleSearch(e.target.value);
+        }}
+        defaultValue={searchParams.get('query')?.toString()}
+      />
     </div>
   )
 }

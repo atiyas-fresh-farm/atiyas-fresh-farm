b9906cb19b72d81a9ed02009f1641403473eda36
Rayyan Ahmed <5747484+rayyanQ@users.noreply.github.com>
2024-10-29 23:10:46 -0400

    style the order-placed page for mobile

--- src/app/order-placed/page.tsx
+++ src/app/order-placed/page.tsx
@@ -5,16 +5,16 @@ import Link from "next/link";
 const OrderPlaced = () => {
   return (
     <div className="w-full flex justify-center">
-      <main className="container flex flex-col justify-start items-start pt-10">
+      <main className="container flex flex-col justify-start items-start pt-10 px-2 lg:px-0">
         <H2>Order Placed</H2>
-        <div className="w-full flex flex-row justify-between items-center mb-4">
+        <div className="w-full flex flex-row flex-wrap justify-between items-center mb-4">
           <P>
             Thank you for placing your order. You will receive an email with your order details. <br />
             We will call if any of the items you ordered are out of stock.
           </P>
           <Badge>Confirming your order</Badge>
         </div>
-        <div className="flex flex-row space-x-4">
+        <div className="flex flex-row space-x-4 mb-8">
           <Link href="/" className="underline">Continue shopping</Link>
           <Link href="/order-history" className="underline">Order History</Link>
         </div>

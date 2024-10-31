16eb4f155b7fb7a98ff609887c438d13546c18a3
Rayyan Ahmed <5747484+rayyanQ@users.noreply.github.com>
2024-10-30 20:48:39 -0400

    update env variable name

--- src/lib/shopify/client.ts
+++ src/lib/shopify/client.ts
@@ -3,7 +3,7 @@ import {createStorefrontApiClient} from '@shopify/storefront-api-client';
 const client = createStorefrontApiClient({
   storeDomain: process.env.SHOPIFY_STORE_DOMAIN ?? '',
   apiVersion: '2024-07',
-  publicAccessToken: process.env.NEXT_PUBLIC_SHOPIFY_STOREFRONT_ACCESS_TOKEN,
+  publicAccessToken: process.env.SHOPIFY_STOREFRONT_ACCESS_TOKEN,
 });
 
 export { client as shopifyClient };

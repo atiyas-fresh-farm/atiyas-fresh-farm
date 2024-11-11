a9f565650da666a03014d57549d06f90a8eba681
Rayyan Ahmed <5747484+rayyanQ@users.noreply.github.com>
2024-11-11 13:32:47 -0500

    add vercel speed analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Noto_Sans } from 'next/font/google';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import { Header } from "@/components/header";
 import { Footer } from "@/components/footer";
 import { ThemeProvider } from "@/components/theme-provider"
@@ -49,6 +50,7 @@ export default async function RootLayout({
             <Toaster closeButton />
           </CartProvider>
         </ThemeProvider>
+        <SpeedInsights />
       </body>
     </html>
   );

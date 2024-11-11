5681fcdf020f6685fbc8e5ae94989c58173b0f87
Rayyan Ahmed <5747484+rayyanQ@users.noreply.github.com>
2024-11-11 11:31:49 -0500

    restyle the footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -74,13 +74,13 @@ const Footer = () => {
     <footer className="w-full h-full flex flex-col justify-center items-center border-t bg-brand text-neutral-50">
       <div className="container flex flex-col justify-center items-center px-4 xl:px-0 py-8">
 
-        <div className="w-full flex flex-col lg:flex-row justify-between items-center lg:items-start">
+        <div className="w-full grid grid-rows-3 lg:grid-rows-1 lg:grid-cols-3 justify-between items-center lg:items-start">
 
-          <div className="w-96 flex flex-col justify-start items-center lg:items-start space-y-8">
+          <div className="w-96 col-span-1 flex flex-col justify-start items-center lg:items-start space-y-8">
             <Link href="/">
               <Image src="/logo-white-black.png" width={200} height={90} alt="Atiyas Fresh Farm Logo" />
             </Link>
-            <P className="text-center lg:text-left">
+            <P className="text-center lg:text-justify">
               Atiya&apos;s Fresh Farm is a fresh addition in the mosaic of the Canadian Grocery world with a presence 22,000 Sq.
               Feet store area. Its meet section consists of 5,000 Sq. Feet. We are serving nothing but 100% Halal items in our store.
             </P>
@@ -94,41 +94,41 @@ const Footer = () => {
             </div>
           </div>
 
-          <div className="w-full h-full flex flex-col lg:flex-row lg:space-x-16 items-center lg:items-start justify-end">
-
-            <div className="flex flex-col justify-start items-center mt-16 lg:mt-4">
-              <H4>Categories</H4>
-              <div className="flex flex-col lg:flex-row lg:space-x-16 items-center lg:items-start justify-end">
-                <div className="flex flex-col justify-start items-center lg:items-start mt-4 space-y-4">
-                  {
-                    categories.slice(0, 5).map((category, index) => (
-                      <Link className={textHoverEffect} href={`/${category.handle}`} key={category.handle + index}>
-                        {category.title}
-                      </Link>
-                    ))
-                  }
-                </div>
-                <div className="flex flex-col justify-start items-center lg:items-start mt-4 space-y-4">
-                  {
-                    categories.slice(5, 10).map((category, index) => (
-                      <Link className={textHoverEffect} href={`/${category.handle}`} key={category.handle + index}>
-                        {category.title}
-                      </Link>
-                    ))
-                  }
-                </div>
-                <div className="flex flex-col justify-start items-center lg:items-start mt-4 space-y-4">
+          <div className="col-span-1 flex flex-col justify-start items-center mt-16 lg:mt-4 px-2">
+            <H4>Categories</H4>
+            <div className="flex flex-col lg:flex-row items-center lg:items-start justify-end">
+              <div className="flex flex-col justify-start items-center text-center mt-4 space-y-4">
                 {
-                    categories.slice(10, 15).map((category, index) => (
-                      <Link className={textHoverEffect} href={`/${category.handle}`} key={category.handle + index}>
-                        {category.title}
-                      </Link>
-                    ))
-                  }
-                </div>
+                  categories.slice(0, 5).map((category, index) => (
+                    <Link className={textHoverEffect} href={`/${category.handle}`} key={category.handle + index}>
+                      {category.title}
+                    </Link>
+                  ))
+                }
+              </div>
+              <div className="flex flex-col justify-start items-center text-center mt-4 space-y-4">
+                {
+                  categories.slice(5, 10).map((category, index) => (
+                    <Link className={textHoverEffect} href={`/${category.handle}`} key={category.handle + index}>
+                      {category.title}
+                    </Link>
+                  ))
+                }
+              </div>
+              <div className="flex flex-col justify-start items-center text-center mt-4 space-y-4">
+              {
+                categories.slice(10, 15).map((category, index) => (
+                  <Link className={textHoverEffect} href={`/${category.handle}`} key={category.handle + index}>
+                    {category.title}
+                  </Link>
+                ))
+              }
               </div>
             </div>
-            <div className="flex flex-col justify-start items-center lg:items-start mt-16 lg:mt-4 space-y-4">
+          </div>
+          
+          <div className="col-span-1 flex flex-col justify-start items-center lg:items-end">
+            <div className="mt-16 lg:mt-4 space-y-4">
               <H4>Contact Details</H4>
               <P>+1 416-901-1333</P>
               <P>info@atiyasfreshfarm.com</P>
@@ -138,8 +138,8 @@ const Footer = () => {
               <P>Scarborough, Ontario</P>
               <P>Canada - M1P 4Y1</P>
             </div>
-
           </div>
+
         </div>
 
         <div className="w-full flex justify-center items-center pt-16 lg:pt-2">

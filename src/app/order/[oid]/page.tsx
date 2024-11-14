5a16475d4ec1b8c4b4f1347b2bc6dfb1f98d915a
Rayyan Ahmed <5747484+rayyanQ@users.noreply.github.com>
2024-11-14 13:09:28 -0500

    print order details on the page

--- src/app/order/[oid]/page.tsx
+++ src/app/order/[oid]/page.tsx
@@ -2,30 +2,9 @@ import { MoveLeft } from "lucide-react";
 import { H2, P } from "@/components/ui/typography";
 import { Button } from "@/components/ui/button";
 import { CartCalculation } from "@/components/cart/cart-calculation";
-//import { getOrderDetails } from "@/components/customer/actions";
-import { getOrder } from "@/lib/shopify/customer";
-import { ShopifyOrder, CustomerToken } from "@/lib/shopify/types";
-import { cookies } from 'next/headers';
+import { getOrderDetails } from "@/components/customer/actions";
+import { Order as OrderType } from "@/lib/shopify/types";
 import Link from "next/link";
-
-
-async function getOrderDetails(orderId: string): Promise<unknown> {
-  const customerTokenString = (await cookies()).get('customerToken')?.value;
-  const { accessToken } = JSON.parse(customerTokenString!) as CustomerToken;
-
-  //TODO: check if the token is expired. If so, refresh it
-
-  if (!customerTokenString) return "Customer Access Token not set";
-
-  try {
-    const orderDetails = await getOrder(orderId, accessToken);
-    //revalidateTag(TAGS.customer);
-    return orderDetails;
-  } catch (e) {
-    console.error(e);
-    return 'Error retrieving orders list';
-  }
-}
 
 
 const Order = async ({ params }: { params: { oid: string } }) => {
@@ -34,55 +13,7 @@ const Order = async ({ params }: { params: { oid: string } }) => {
 
   // **************************
   //TODO: the values are being printed properly on the server, but for some reason the client is not able to get the values
-  const orderDetails = await getOrderDetails(orderId) as ShopifyOrder;
-  console.log(orderDetails);
-  const products = [
-    {
-      count: 2,
-      product: {
-        title: "Mosambi 100g",
-        imageURL: "/fruits-vegetables/mosambi.jpeg",
-        altText: "Mosambi",
-        price: 2.3
-      },
-    },
-    {
-      count: 5,
-      product: {
-        title: "Apple Shimla 1pc",
-        imageURL: "/fruits-vegetables/apple-shimla.jpeg",
-        altText: "apples",
-        price: 1.8
-      },
-    },
-    {
-      count: 1,
-      product: {
-        title: "Dragon Fruits 1pc",
-        imageURL: "/fruits-vegetables/drangon-fruit.jpeg",
-        altText: "Dragon Fruit",
-        price: 4.12
-      },
-    },
-    {
-      count: 2,
-      product: {
-        title: "Garlic 100g",
-        imageURL: "/fruits-vegetables/garlic.jpeg",
-        altText: "Garlic",
-        price: 2.3
-      },
-    }
-  ];
-  const order = {
-    id: "23242",
-    date: "Oct 28, 2024",
-    cost: 232.32,
-    subtotal: 220.23,
-    tax: 10.09,
-    delivery: 2.09,
-    products: products
-  }
+  const orderDetails = await getOrderDetails(orderId) as OrderType;
 
   return (
     <div className="w-full flex justify-center">
@@ -95,17 +26,13 @@ const Order = async ({ params }: { params: { oid: string } }) => {
 
         <div className="w-full flex flex-col justify-start items-start mt-4">
           <p>Order id: {orderDetails?.id}</p>
-          <p>Order date: {order.date}</p>
+          <p>Order date: {orderDetails.createdAt.toString().slice(0, 10)}</p>
           <p>Total cost: ${orderDetails?.totalPrice?.amount}</p>
         </div>
 
-        {orderDetails?.lineItems?.toString()}
-
-        {
-          order.products.map((row) => (
-            <span key={row.product.title}>{row.product.title}</span>
-          ))
-        }
+        {orderDetails?.lineItems?.map((product) => (
+          <span key={product.name}>{product.name}</span>
+        ))}
 
         <div className="w-full flex flex-row justify-end items-center mt-8">
           <CartCalculation />

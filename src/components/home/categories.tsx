a63b76c12e240aefeb00fa9fd03b1ccb2464b206
Rayyan Ahmed <5747484+rayyanQ@users.noreply.github.com>
2024-11-29 12:22:16 -0500

    adjust the height of the hero section

--- src/components/home/categories.tsx
+++ src/components/home/categories.tsx
@@ -14,17 +14,17 @@ const Categories = (
     <div id="explore-categories" className="mb-10 px-2 xl:px-0">
 
       <H2 className="w-full text-center">{title}</H2>
-      <div className="w-full flex flex-row flex-wrap justify-center items-center my-4 px-4">
+      <div className="w-full flex flex-row flex-wrap justify-center items-start my-4 px-4">
         {
           // TODO: style for mobile - 2 columns min
           categories.map((category, index) => (
             <Link href={`/${category.handle}`} key={category.handle + index} className="flex flex-col justify-center items-center mb-4">
-              <div className="w-60 h-60 relative rounded-md m-2 overflow-hidden">
+              <div className="w-40 h-40 md:w-60 md:h-60 relative rounded-md m-2 overflow-hidden">
                 <Image src={category.imageSrc} fill={true} alt={category.title}
                   className={`rounded-xl object-contain transition-transform duration-300 ease-in-out hover:scale-105 scale-100`}
                 />
               </div>
-              <H3>{category.title}</H3>
+              <H3 className="max-w-40 md:max-w-60 text-center">{category.title}</H3>
             </Link>
           ))
         }
--- src/components/home/hero.tsx
+++ src/components/home/hero.tsx
@@ -13,13 +13,13 @@ import {
 const Hero = () => {
 
   return (
-    <div className="w-full h-[350px] lg:h-[500px] xl:h-[650px] max-h-[65vh] border mb-12">
-      <Carousel className="w-full h-[350px] lg:h-[650px]">
+    <div className="w-full h-[250px] md:h-[400px] lg:h-[500px] xl:h-[650px] max-h-[65vh] border mb-12">
+      <Carousel className="w-full h-[250px] md:h-[400px] lg:h-[500px]">
         <CarouselContent>
-          <CarouselItem className="w-full h-[350px] lg:h-[650px] max-h-[65vh] relative">
+          <CarouselItem className="w-full h-[250px] md:h-[400px] lg:h-[500px] max-h-[65vh] relative">
             <Image src="/home/welcome-banner.png" style={{ objectFit: 'cover' }} fill={true} priority={true} alt="Welcome banner" />
           </CarouselItem>
-          <CarouselItem className="w-full h-[350px] lg:h-[650px] max-h-[65vh] relative">
+          <CarouselItem className="w-full h-[250px] md:h-[400px] lg:h-[500px] max-h-[65vh] relative">
             <Image src="/home/home-banner.png" fill={true} objectFit="cover" alt="Home banner" />
           </CarouselItem>
         </CarouselContent>

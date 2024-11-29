89163a5eccf3bddeba179e964bd602085a17858e
Rayyan Ahmed <5747484+rayyanQ@users.noreply.github.com>
2024-11-29 16:03:10 -0500

    update hero height styling

--- src/components/home/hero.tsx
+++ src/components/home/hero.tsx
@@ -12,14 +12,16 @@ import {
 //TODO: make the props required
 const Hero = () => {
 
+  const heightStyling = `h-[250px] md:h-[400px] lg:h-[500px] xl:h-[650px] max-h-[65vh]`;
+
   return (
-    <div className="w-full h-[250px] md:h-[400px] lg:h-[500px] xl:h-[650px] max-h-[65vh] border mb-12">
-      <Carousel className="w-full h-[250px] md:h-[400px] lg:h-[500px]">
+    <div className={`w-full ${heightStyling} border mb-12`}>
+      <Carousel className={`w-full ${heightStyling}`}>
         <CarouselContent>
-          <CarouselItem className="w-full h-[250px] md:h-[400px] lg:h-[500px] max-h-[65vh] relative">
+          <CarouselItem className={`w-full ${heightStyling} relative`}>
             <Image src="/home/welcome-banner.png" style={{ objectFit: 'cover' }} fill={true} priority={true} alt="Welcome banner" />
           </CarouselItem>
-          <CarouselItem className="w-full h-[250px] md:h-[400px] lg:h-[500px] max-h-[65vh] relative">
+          <CarouselItem className={`w-full ${heightStyling} relative"`}>
             <Image src="/home/home-banner.png" fill={true} objectFit="cover" alt="Home banner" />
           </CarouselItem>
         </CarouselContent>
